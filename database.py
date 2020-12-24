fe6ace85adac491f5d16b9de7cf3a2b3b78218f7
rbaltrusch <41642326+rbaltrusch@users.noreply.github.com>
2020-12-24 13:37:40 +0100

    added code to create_products_table, added create_detailed_transactions_table, renamed create_transaction_table to create_transactions_table, changed create_session_table foreign key restriction from ON DELETE RESTRICT TO CASCADE, added functions add_product, get_last_added_transaction_id_from_transactions_table, changed code in add_transaction to take into account queried product cost from products table, added update_user and update_user_by_user_email, query_user_id_from_user_email, query_product_ids_from_product_table, query_product_price_from_product_table, query_user_data_by_user_email, query_user_data, query_pw_hash_and_salt_by_user_email

--- database.py
+++ database.py
@@ -7,18 +7,73 @@ Created on Sun Nov 22 14:31:48 2020
 
 import crypto
 
-def get_user_ids_from_user_table(cursor):
-    user_ids = [user_id[0] for user_id in cursor.execute('SELECT user_id FROM users')]
+def query_user_id_from_user_email(cursor, user_email):
+    command = 'SELECT user_id FROM users WHERE email_address = ?'
+    user_ids = [user_id for user_id, *_ in cursor.execute(command, [user_email])]
+    return user_ids[0] if user_ids else None
+
+def query_user_ids_from_user_table(cursor):
+    '''Returns all user_ids found in users table as a list'''
+    user_ids = [user_id for user_id, *_ in cursor.execute('SELECT user_id FROM users')]
     return user_ids
 
-def verify_session_id(cursor, session_id):
-    data = cursor.execute('SELECT session_id FROM sessions WHERE session_id = ?', session_id)
+def query_product_ids_from_product_table(cursor):
+    '''Returns all product ids found in products table as a list'''
+    product_ids = [product_id for product_id, *_ in cursor.execute('SELECT product_id FROM products''')]
+    return product_ids
+
+def query_product_price_from_product_table(cursor, product_ids):
+    '''Returns all prices found for the specified product ids as a list'''
+    joined_product_ids = ','.join([str(product_id) for product_id in product_ids])
+    joined_product_ids_str = f"({joined_product_ids})"
+
+    command = 'SELECT price FROM products WHERE product_id IN {}'.format(joined_product_ids_str)
+    product_prices = [product_price for product_price, *_ in cursor.execute(command)] #joined_product_ids_str
+    return product_prices
+
+def query_user_data_by_user_email(cursor, user_email):
+    data = cursor.execute('SELECT * FROM users where email_address = ?', [user_email])
+    return list(list(data)[0])
+
+def query_user_data(cursor, user_id):
+    data = cursor.execute('SELECT * FROM users WHERE user_id = ?', [str(user_id)])
+    return list(list(data)[0])
+
+def query_pw_hash_and_salt_by_user_email(cursor, user_email):
+    command = 'SELECT pw_salt, pw_hash FROM users WHERE email_address = ?'
+    data = cursor.execute(command, [user_email])
+    return list(list(data)[0])
+
+def get_last_added_transaction_id_from_transactions_table(cursor):
+    transaction_id, *_ = [id_ for id_, *_ in cursor.execute('''SELECT last_insert_rowid()''')]
+    return transaction_id
+
+def verify_session_id(cursor, session_id, user_id):
+    data = cursor.execute('SELECT session_id FROM sessions WHERE session_id = ? AND user_id = ?', [session_id, user_id])
+    data = [d for d, *_ in data]
     verified = data and data[0] == session_id
+    if not verified:
+        verified = False
+    return verified
+
+def verify_session_id_by_user_email(cursor, session_id, user_email):
+    command = '''SELECT session_id FROM sessions
+                  WHERE session_id = ?
+                  AND user_id IN
+                  (
+                      SELECT user_id FROM users
+                      WHERE email_address = ?)'''
+  
+    data = cursor.execute(command, [session_id, user_email])
+    data = [d for d, *_ in data]
+    verified = data and data[0] == session_id
+    if not verified:
+        verified = False
     return verified
 
 def add_user(cursor, user_data):
     command = '''INSERT INTO users
-                (first_name, last_name, gender, join_date, dob, email_address, pw_salt, pw)
+                (first_name, last_name, gender, join_date, dob, email_address, pw_salt, pw_hash)
                 VALUES (?, ?, ?, ?, ?, ?, ?, ?)'''
 
     #hash password
@@ -28,18 +83,59 @@ def add_user(cursor, user_data):
 
     cursor.execute(command, user_data)
 
-def update_user(cursor, user_data):
-    '''user_data needs to start with user_id and the rest be a full set of user_data'''
-    command = '''UPDATE users '''
+def update_user(cursor, user_data, user_id):
+    '''user_data needs to be a full set of user_data, without the user_id, which is passed separately'''
+    user_data.append(user_id)
+    command = '''UPDATE users
+                SET
+                    first_name = ?,
+                    last_name = ?,
+                    gender = ?,
+                    join_date = ?,
+                    dob = ?,
+                    email_address = ?,
+                    pw_salt = ?,
+                    pw_hash = ?
+                WHERE user_id = ?'''
     cursor.execute(command, user_data)
 
-def add_transaction(cursor, transaction_data):
+def update_user_by_user_email(cursor, user_data, user_email):
+    '''user_data needs to be a full set of user_data, without the user_id, which is passed separately'''
+    user_data.append(user_email)
+    command = '''UPDATE users
+                SET
+                    first_name = ?,
+                    last_name = ?,
+                    gender = ?,
+                    join_date = ?,
+                    dob = ?,
+                    email_address = ?,
+                    pw_salt = ?,
+                    pw_hash = ?
+                WHERE email_address = ?'''
+    cursor.execute(command, user_data)
+
+def add_transaction(cursor, transaction_data, chosen_product_ids):
+    product_prices = query_product_price_from_product_table(cursor, chosen_product_ids)
+    cost = sum(product_prices)
+    transaction_data.append(cost)
+
     command = 'INSERT INTO transactions (user_id, date, cost) VALUES (?, ?, ?)'
     cursor.execute(command, transaction_data)
+    
+    transaction_id = get_last_added_transaction_id_from_transactions_table(cursor)
 
+    command = 'INSERT INTO detailed_transactions (transaction_id, product_id) VALUES (?, ?)'
+    for chosen_product_id in chosen_product_ids:
+        cursor.execute(command, [transaction_id, chosen_product_id])
+    
 def add_session(cursor, session_data):
     command = 'INSERT INTO sessions (session_id, user_id, timestamp) VALUES (?, ?, ?)'
     cursor.execute(command, session_data)
+
+def add_product(cursor, product_data):
+    command = 'INSERT INTO products (name, price) VALUES (?, ?)'
+    cursor.execute(command, product_data)
 
 def create_user_table(cursor):
     #remove table
@@ -56,7 +152,7 @@ def create_user_table(cursor):
                                      CAST(strftime('%s', dob)  AS  integer)),
                      email_address TEXT NOT NULL UNIQUE,
                      pw_salt NOT NULL,
-                     pw NOT NULL
+                     pw_hash NOT NULL
                      CHECK (email_address LIKE '%_@_%._%')
                      )''')
  
@@ -66,7 +162,8 @@ def create_user_table(cursor):
     #create index based on user_id
     cursor.execute('''CREATE INDEX idx_user_id ON users(user_id)''')
 
-def create_transaction_table(cursor):
+
+def create_transactions_table(cursor):
     #remove table
     cursor.execute('''DROP TABLE IF EXISTS transactions''')
 
@@ -88,8 +185,37 @@ def create_transaction_table(cursor):
     #create index based on user_id
     cursor.execute('''CREATE INDEX idx_user_id ON transactions(user_id)''')
 
+
+def create_detailed_transactions_table(cursor):
+    #remove table
+    cursor.execute('''DROP TABLE IF EXISTS detailed_transactions''')
+
+    #create table
+    cursor.execute('''CREATE TABLE detailed_transactions (
+                    transaction_id INTEGER,
+                    product_id INTEGER,
+                    FOREIGN KEY (transaction_id)
+                        REFERENCES transactions (transaction_id)
+                        ON UPDATE CASCADE
+                        ON DELETE RESTRICT,
+                    FOREIGN KEY (product_id)
+                        REFERENCES products (product_id)
+                        ON UPDATE CASCADE
+                        ON DELETE RESTRICT)
+                    ''')
+
+
 def create_products_table(cursor):
-    pass
+    #remove table
+    cursor.execute('''DROP TABLE IF EXISTS products''')
+
+    #create table
+    cursor.execute('''CREATE TABLE products (
+                    product_id INTEGER PRIMARY KEY,
+                    name TEXT,
+                    price REAL NOT NULL
+                    )''')
+
 
 def create_sessions_table(cursor):
     #remove table
@@ -103,5 +229,5 @@ def create_sessions_table(cursor):
                     FOREIGN KEY (user_id)
                         REFERENCES users (user_id)
                         ON UPDATE CASCADE
-                        ON DELETE RESTRICT
+                        ON DELETE CASCADE
                     )''')
