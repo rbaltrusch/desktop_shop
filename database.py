6104fcde2871010c1b17aba0100a822565ddeed2
rbaltrusch <41642326+rbaltrusch@users.noreply.github.com>
2020-12-25 22:02:01 +0100

    sanitized input in query_product_price_from_product_table_by_product_table_by_product_ids, added functions to query product_data, fixed a bug in user query data return for searches returning no results, limited the data that can be updated or queried from the users table, salt is now generated here whenever adding a new user, instead of the salt being passed as an input to the database function

--- database.py
+++ database.py
@@ -22,27 +22,51 @@ def query_product_ids_from_product_table(cursor):
     product_ids = [product_id for product_id, *_ in cursor.execute('SELECT product_id FROM products''')]
     return product_ids
 
+def query_product_data_from_product_table(cursor):
+    data = [list(row) for row in cursor.execute('SELECT * FROM products')]
+    return data
+
+def query_product_data_from_product_table_by_product_ids(cursor, product_ids):
+
+    #sanitize input and make sure all product ids are only numeric
+    if not all(product_id.isnumeric() for product_id in product_ids):
+        return []
+
+    joined_product_ids = ','.join([str(product_id) for product_id in product_ids])
+    joined_product_ids_str = f"({joined_product_ids})"
+    command = 'SELECT * FROM products WHERE product_id IN {}'.format(joined_product_ids_str)
+    data = [list(row) for row in cursor.execute(command)]
+    return data
+
 def query_product_price_from_product_table(cursor, product_ids):
     '''Returns all prices found for the specified product ids as a list'''
+
+    #sanitize input and make sure all product ids are only numeric
+    if not all(product_id.isnumeric() for product_id in product_ids):
+        return []
+
     joined_product_ids = ','.join([str(product_id) for product_id in product_ids])
     joined_product_ids_str = f"({joined_product_ids})"
 
     command = 'SELECT price FROM products WHERE product_id IN {}'.format(joined_product_ids_str)
-    product_prices = [product_price for product_price, *_ in cursor.execute(command)] #joined_product_ids_str
+    product_prices = [product_price for product_price, *_ in cursor.execute(command)]
     return product_prices
 
 def query_user_data_by_user_email(cursor, user_email):
-    data = cursor.execute('SELECT * FROM users where email_address = ?', [user_email])
-    return list(list(data)[0])
+    data = cursor.execute('SELECT first_name, last_name, gender, join_date, email_address, dob FROM users where email_address = ?', [user_email])
+    data = list(data)
+    return list(data[0]) if data else []
 
 def query_user_data(cursor, user_id):
-    data = cursor.execute('SELECT * FROM users WHERE user_id = ?', [str(user_id)])
-    return list(list(data)[0])
+    data = cursor.execute('SELECT first_name, last_name, gender, join_date, email_address, dob FROM users WHERE user_id = ?', [str(user_id)])
+    data = list(data)
+    return list(data[0]) if data else []
 
 def query_pw_hash_and_salt_by_user_email(cursor, user_email):
     command = 'SELECT pw_salt, pw_hash FROM users WHERE email_address = ?'
     data = cursor.execute(command, [user_email])
-    return list(list(data)[0])
+    data = list(data)
+    return list(data[0]) if data else []
 
 def get_last_added_transaction_id_from_transactions_table(cursor):
     transaction_id, *_ = [id_ for id_, *_ in cursor.execute('''SELECT last_insert_rowid()''')]
@@ -77,7 +101,8 @@ def add_user(cursor, user_data):
                 VALUES (?, ?, ?, ?, ?, ?, ?, ?)'''
 
     #hash password
-    *user_data, salt, password = user_data
+    *user_data, password = user_data
+    salt = crypto.generate_new_salt()
     hashed_password = crypto.hash_string(password, salt)
     user_data = user_data + [salt, hashed_password]
 
@@ -91,11 +116,8 @@ def update_user(cursor, user_data, user_id):
                     first_name = ?,
                     last_name = ?,
                     gender = ?,
-                    join_date = ?,
                     dob = ?,
-                    email_address = ?,
-                    pw_salt = ?,
-                    pw_hash = ?
+                    email_address = ?
                 WHERE user_id = ?'''
     cursor.execute(command, user_data)
 
@@ -107,11 +129,8 @@ def update_user_by_user_email(cursor, user_data, user_email):
                     first_name = ?,
                     last_name = ?,
                     gender = ?,
-                    join_date = ?,
                     dob = ?,
-                    email_address = ?,
-                    pw_salt = ?,
-                    pw_hash = ?
+                    email_address = ?
                 WHERE email_address = ?'''
     cursor.execute(command, user_data)
 
