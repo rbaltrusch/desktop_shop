167fad91cb8bbd6747230a4e55d7349fed27a155
Richard Baltrusch <richard@baltrusch.net>
2021-12-05 13:28:50 +0100

    Fix edit user data

--- desktop_shop/gui/callbacks.py
+++ desktop_shop/gui/callbacks.py
@@ -105,7 +105,7 @@ def edit_user_data():
         user_data = [first_name, last_name, gender, dob, user_email]
 
         with db_conn as cursor:
-            new_session_id, *_ = server.update_user(cursor, user_data, user_email,
+            new_session_id, *_ = server.update_user(cursor, user_data, user_data.email,
                                                     user_email=user_email, session_id=session_id)
 
         app.data['session_id'] = new_session_id
