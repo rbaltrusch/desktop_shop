3f3b0710a9d6aa62513769272f41398b23755312
Richard Baltrusch <richard@baltrusch.net>
2021-12-05 14:54:57 +0100

    Close db after gui run

--- desktop_shop/main.py
+++ desktop_shop/main.py
@@ -5,9 +5,12 @@ Created on Mon Feb  1 09:52:44 2021
 @author: Korean_Crimson
 """
 
-from gui import init, app
+from gui import init, app, db_conn
 
-init.init()
-app.views_dict['main_menu'].pack()
-app.views_dict['home'].pack()
-app.mainloop()
+try:
+    init.init()
+    app.views_dict['main_menu'].pack()
+    app.views_dict['home'].pack()
+    app.mainloop()
+finally:
+    db_conn.close()
