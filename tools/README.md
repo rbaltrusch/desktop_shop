0c4fcffe5fcfa84b46bc24c7efdae407fb4da415
Richard Baltrusch <richard@baltrusch.net>
2021-12-18 13:11:55 +0100

    Refactor query_database

--- tools/README.md
+++ tools/README.md
@@ -1,1 +1,17 @@
-Copy the file query_database.py into the root code directory desktop_shop to run commands on the main.db database without authentification.
+# How to use
+
+## Query database
+
+The file query_database.py is provided to explore the contents of the main.db database without authentification.
+A number of example functions are provided in the file to query the contents of the users, transactions and products tables.
+
+To run the file, open a terminal and run the following commands:
+```
+cd tools
+python query_database.py
+```
+
+## Update database
+
+The file update_database.py is provided when changes to some or all the data in the database need to be made internally, such as when changing the format of some of the data in it.
+Outside of that, it has little use and should be used sparingly.
--- tools/query_database.py
+++ tools/query_database.py
@@ -4,54 +4,95 @@ Created on Sat Nov 14 17:09:26 2020
 
 @author: Korean_Crimson
 """
+import sqlite3
 
-from util import DataBaseConnection, timeit
-
-@timeit
-def user_test_1(cursor):
-    #get name, number of transactions, total cost of all male users that spent over 1000 euro and were born in 2004
-    data = cursor.execute('SELECT l.first_name, l.last_name, l.dob, COUNT(*), SUM(r.cost) FROM users l INNER JOIN transactions r ON l.user_id = r.user_id WHERE l.gender="m" AND l.dob GLOB "2004*" GROUP BY r.user_id HAVING SUM(r.cost) > 1000 ORDER BY SUM(r.cost)')
+def query_power_users(cursor):
+    """get name, number of transactions, total cost of all male users that spent
+    over 1000 euro and were born in 2004.
+    """
+    data = cursor.execute("""SELECT l.first_name, l.last_name, l.dob, COUNT(*), SUM(r.cost)
+                          FROM users l INNER JOIN transactions r ON l.user_id = r.user_id
+                          WHERE l.gender="m" AND l.dob GLOB "2004*" GROUP BY r.user_id
+                          HAVING SUM(r.cost) > 1000 ORDER BY SUM(r.cost) DESC""")
     for row in data:
         print(row)
 
-with DataBaseConnection('main.db') as cursor:
-#    data = cursor.execute('''SELECT name FROM sqlite_master where type="table"''')
-#    for row in data:
-#        print(row)
-
-#    data = cursor.execute('''SELECT SUM(price) FROM products WHERE product_id in (SELECT d.product_id FROM detailed_transactions d INNER JOIN transactions t ON d.transaction_id = t.transaction_id WHERE t.transaction_id = 1754)''')
-#    for row in data:
-#        print(row)
-
-#    data = cursor.execute('''SELECT * FROM sessions''')
-#    for row in data:
-#        print(row)
-
-#    data = cursor.execute('SELECT * FROM products ORDER BY price DESC')
-#    for row in data:
-#        print(row)
-
-#    data = cursor.execute('SELECT * FROM users LIMIT 100')
-#    for row in data:
-#        print(row)
-
-    data = cursor.execute('SELECT * FROM transactions WHERE user_id IN (SELECT user_id FROM users WHERE email_address = ?)', ['FLLKX1924796796@emailook.com']) #h2@e.com
+def query_transactions_for_user(cursor, email):
+    """Query transactions from a user by the specified email"""
+    data = cursor.execute("""SELECT * FROM transactions WHERE user_id IN
+                          (SELECT user_id FROM users WHERE email_address = ?)""", (email,))
     for row in data:
         print(row)
 
-#
-#    data = cursor.execute('SELECT date, cost, user_id FROM transactions ORDER BY cost ASC LIMIT 10')
-#    for row in data:
-#        pass
-#    
-#    data = cursor.execute('SELECT user_id, COUNT(*), SUM(cost) FROM transactions GROUP BY user_id HAVING COUNT(*) > 20 ORDER BY SUM(cost) DESC')
-#    for row in data:
-#        print(row)
-#
-#    data = cursor.execute('SELECT user_id FROM users WHERE last_name LIKE "P%s" ORDER BY first_name')
-#    user_id = [point for point in data][0]
-#    data = cursor.execute('SELECT * FROM transactions WHERE user_id IN (?)', user_id)
-#    for row in data:
-#        print(row)
-#
-#    user_test_1(cursor)
+def query_transactions_for_user_by_pattern(cursor, pattern):
+    """Retrieve transactions for user where the last name matches the specified sqlite3 pattern.
+    e.g. the pattern P%r would match Peter.
+    """
+    data = cursor.execute("""SELECT user_id FROM users
+                          WHERE last_name LIKE ? ORDER BY first_name""", (pattern,))
+    data = list(data)
+    if not data:
+        return
+
+    data = cursor.execute('SELECT * FROM transactions WHERE user_id IN (?)', data[0])
+    for row in data:
+        print(row)
+
+def query_transaction_price(cursor, transaction_id):
+    """Returns the total price of the transaction specified by the passed id"""
+    data = cursor.execute("""SELECT SUM(price) FROM products
+                          WHERE product_id in
+                          (SELECT d.product_id FROM detailed_transactions d
+                           INNER JOIN transactions t ON d.transaction_id = t.transaction_id
+                           WHERE t.transaction_id = ?)""", (transaction_id,))
+    for row in data:
+        print(row)
+
+def list_some_users(cursor, limit=100):
+    """Lists the first users in the user table, limited to the specified amount"""
+    data = cursor.execute('SELECT * FROM users LIMIT ?', (limit,))
+    for row in data:
+        print(row)
+
+
+def list_sqlite_tables(cursor):
+    """Lists all the tables found the database"""
+    data = cursor.execute('''SELECT name FROM sqlite_master where type="table"''')
+    for row in data:
+        print(row)
+
+def list_all_sessions(cursor):
+    """Lists all data in the sessions table"""
+    data = cursor.execute('''SELECT * FROM sessions''')
+    for row in data:
+        print(row)
+
+def list_products(cursor):
+    """Lists products, descending by price"""
+    data = cursor.execute('SELECT * FROM products ORDER BY price DESC')
+    for row in data:
+        print(row)
+
+def list_cheapest_transactions(cursor, limit=10):
+    """Lists the cheapest transactions, limited to the amount specified"""
+    data = cursor.execute("""SELECT date, cost, user_id FROM transactions
+                          ORDER BY cost ASC LIMIT ?""", (limit, ))
+    for row in data:
+        print(row)
+
+def list_biggest_consumers(cursor, orders=20):
+    """Lists the users that have more than the specified amount of orders"""
+    data = cursor.execute("""SELECT user_id, COUNT(*), SUM(cost) FROM transactions
+                          GROUP BY user_id HAVING COUNT(*) > ?
+                          ORDER BY SUM(cost) DESC""", (orders,))
+    for row in data:
+        print(row)
+
+
+def main():
+    """Main function"""
+    with sqlite3.connect('../desktop_shop/main.db') as cursor:
+        query_transactions_for_user_by_pattern(cursor, 'A%r')
+
+if __name__ == '__main__':
+    main()
